49fb3d4eadc6e8cc36cde6de7c79ffb0c868b687
윤주원 <yunjuwon@yunjuwons-Mac-mini.local>
2022-02-07 16:54:13 +0900

    init vue3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import axios from 'axios'
+
+// axios.get('http://localhost:1337/api/restaurants').then(response => {
+//   console.log(response);
+// });
 
 createApp(App).mount('#app')
