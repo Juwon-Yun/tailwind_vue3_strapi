6695c793ea6e829605e4fc9a143981c0bc6731ca
윤주원 <yunjuwon@yunjuwons-Mac-mini.local>
2022-02-08 13:11:46 +0900

    tailwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
+
 // import axios from 'axios'
 
 // axios.get('http://localhost:1337/api/restaurants').then(response => {
